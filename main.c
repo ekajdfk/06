f47a9d27e914811de5e4a51b2b62ea840c837069
ekajdfk <yoosy320@gmail.com>
2023-10-13 09:42:16 +0900

    5

--- main.c
+++ main.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
+int factorial(int n)
 {
-  return (a*a);
+  int res = 1;
+  int i;
+  
+  for(i=1; i<=n; i++)
+        res = res*i;
+        
+  return res;  
+}
+
+int combination(int n, int r)
+{
+  int div1, div2;
+  
+  div1 = factorial(n);
+  div2 = factorial(n-r)*factorial(r);
+  
+  return div1/div2;
 }
 
 int main()
 {
-  int a = 2;
-  a = square(a);
-  printf("a = %i\n", a);
+  int n, r;
+  int result;
+  
+  printf("input n : ");
+  scanf("%d", &n);
+  
+  printf("input r : ");
+  scanf("%d", &r);
+  
+  result = combination(n, r);
+  printf("result is %i\n", result);
   
   system("PAUSE");
   return 0;
